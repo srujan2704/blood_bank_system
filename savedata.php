fe3f31327b99d994ef7808dbc0d75cfc24600f88
ananyakumar05 <ananya_kumar@icloud.com>
2025-04-16 23:32:50 +0530

    savedata

--- savedata.php
+++ savedata.php
@@ -1,16 +1,27 @@
 <?php
-$name=$_POST['fullname'];
-$number=$_POST['mobileno'];
-$email=$_POST['emailid'];
-$age=$_POST['age'];
-$gender=$_POST['gender'];
-$blood_group=$_POST['blood'];
-$address=$_POST['address'];
-$conn=mysqli_connect("localhost","root","","blood_donation") or die("Connection error");
-$sql= "INSERT INTO donor_details(donor_name,donor_number,donor_mail,donor_age,donor_gender,donor_blood,donor_address) values('{$name}','{$number}','{$email}','{$age}','{$gender}','{$blood_group}','{$address}')";
-$result=mysqli_query($conn,$sql) or die("query unsuccessful.");
+$name = $_POST['fullname'];
+$number = $_POST['mobileno'];
+$email = $_POST['emailid'];
+$age = $_POST['age'];
+$gender = $_POST['gender'];
+$blood_group = $_POST['blood'];
+$address = $_POST['address'];
 
-header("Location: http://localhost/BDMS/home.php");
+$conn = new mysqli("localhost", "root", "", "blood_donation");
 
-mysqli_close($conn);
- ?>
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$stmt = $conn->prepare("INSERT INTO donor_details(donor_name, donor_number, donor_mail, donor_age, donor_gender, donor_blood, donor_address) VALUES (?, ?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("sssisss", $name, $number, $email, $age, $gender, $blood_group, $address);
+
+if ($stmt->execute()) {
+    header("Location: http://localhost/Blood-Bank-And-Donation-Management-System/home.php");
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
+?>
